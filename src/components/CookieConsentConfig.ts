505468a9d65711465ba1db98baa6894741148c67
Julian Price <github.com@julianprice.org.uk>
2024-02-20 13:09:34 +0000

    Use cookie preference centre

--- src/components/CookieConsentConfig.ts
+++ src/components/CookieConsentConfig.ts
@@ -6,29 +6,19 @@ export const config: CookieConsentConfig = {
       layout: 'box inline',
       position: 'bottom right',
     },
+    preferencesModal: {
+      layout: 'box',
+      position: 'right',
+      equalWeightButtons: true,
+      flipButtons: false,
+    },
   },
   categories: {
-    functionality: {},
-    analytics: {
-      services: {
-        ga4: {
-          label:
-            '<a href="https://marketingplatform.google.com/about/analytics/terms/us/" target="_blank">Google Analytics 4</a>',
-          onAccept: () => {
-            console.log('ga4 accepted');
-            // TODO: load ga4
-          },
-          onReject: () => {
-            console.log('ga4 rejected');
-          },
-          cookies: [
-            {
-              name: /^_ga/,
-            },
-          ],
-        },
-      },
+    necessary: {
+      readOnly: true,
     },
+    functionality: {},
+    analytics: {},
   },
   language: {
     default: 'en',
@@ -36,14 +26,43 @@ export const config: CookieConsentConfig = {
     translations: {
       en: {
         consentModal: {
-          title: "We use a cookie to collect stats",
+          title: "We use cookies for preferences and statistics",
           description:
-            'This web site uses a cookie to collect and store data about how visitors use the website. This data is anonymous and agregated. You may accept or reject data collection',
-          acceptAllBtn: 'Accept',
-          acceptNecessaryBtn: 'Reject',
-          footer: '<a href="/cookies/">Cookie Policy</a>'
+            'This web site uses cookies to save your preferences and to collect and store statistics about how visitors use the website.',
+          acceptAllBtn: 'Accept all',
+          showPreferencesBtn: 'Manage preferences',
+          footer:
+          '<a href="/cookies/">Cookie Policy</a>',
         },
-        preferencesModal: { sections: [], },
+        preferencesModal: {
+          title: 'Consent Preferences Center',
+          acceptAllBtn: 'Accept all',
+          acceptNecessaryBtn: 'Reject all',
+          savePreferencesBtn: 'Save preferences',
+          closeIconLabel: 'Close modal',
+          serviceCounterLabel: 'Service|Services',
+          sections: [
+            {
+              title: 'Cookie Usage',
+              description:
+                'This web site uses cookies to save your preferences and to collect and store statistics about how visitors use the website.\
+                For more information, see our <a href="/cookies/">Cookie Policy</a>.',
+            },
+            {
+              title:
+                'Your cookie preference <span class="pm__badge">Always Enabled</span>',
+              description:
+                'A cookie is used to remember whether you prefer to accept or reject other cookies. Without this, the cookie messages would keep appearing. This cookie is not observed by the web server.',
+              linkedCategory: 'necessary',
+            },
+            {
+              title: 'Google Analytics Cookies',
+              description:
+                'Google Analytics uses a cookie to collect and store data about how visitors use the website. This data is anonymous and agregated. If you prefer your visit to be excluded from statistics, you may disable Google Analytics cookies.',
+              linkedCategory: 'analytics',
+            },
+          ],
+        },
       },
     },
   },
