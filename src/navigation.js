64f3fe41d49bcf32739bd5e9d9e7940a0287a652
Julian Price <github.com@julianprice.org.uk>
2024-04-02 13:49:07 +0100

    podcastPlatforms list is a parameter of the Subscribe component

--- src/navigation.js
+++ src/navigation.js
@@ -34,14 +34,3 @@ export const footerData = {
     Copyright Ricardo Lopes · All rights reserved.
   `,
 };
-
-// Image files with .svg extension are in src/assets/images/listen
-export const listenData = [
-  { action: 'Watch ', text: 'YouTube', image: 'youtube', url: 'https://www.youtube.com/@TheDissenterRL', ratio: 159/110 },
-  { text: 'Apple Podcasts', image: 'apple', url: 'https://podcasts.apple.com/gb/podcast/the-dissenter/id1451347236' },
-  { text: 'Spotify', image: 'spotify', url: 'https://podcasters.spotify.com/pod/show/thedissenter' },
-  { text: 'Pocket Casts', image: 'pocketcasts', url: 'https://pca.st/5lfr' },
-  { text: 'Overcast', image: 'overcast', url: 'https://overcast.fm/itunes1451347236/the-dissenter' },
-  { text: 'Audible', image: 'audible', url: 'https://www.audible.co.uk/podcast/The-Dissenter/B0CXJMM9RC' },
-  { text: 'RSS', image: 'rss', url: 'https://anchor.fm/s/822ba20/podcast/rss' },
-];
